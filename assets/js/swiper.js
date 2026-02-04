23dccecfd26891e19a5a7370563955d5f1fc9597
nerfall <nerfall10@gmail.com>
2026-02-04 18:22:01 +0200

    update error

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,31 +1,48 @@
-const swiper = new Swiper(".swiper_campaign", {
+const swiper = new Swiper('.swiper_campaign', {
 
-	breakpoints: {
-        320: {
-			slidesPerView: 1,
-			spaceBetween: 16,
-		},
-		640: {
-			slidesPerView: 2,
-			spaceBetween: 16,
-		},
-		1024: {
-			slidesPerView: 3,
-			spaceBetween: 24,
-		},
-		
-	},
+  loop: false,
+  watchOverflow: true,
 
-	navigation: {
+  breakpoints: {
+
+    320: {
+		  spaceBetween: 16,
+      slidesPerView: 1,
+      grid: {
+        rows: 3,
+        fill: 'row',
+      },
+    },
+
+    640: {
+		  spaceBetween: 16,
+      slidesPerView: 2,
+      grid: {
+        rows: 2,
+        fill: 'row',
+      },
+    },
+
+    1024: {
+		  spaceBetween: 24,
+      slidesPerView: 3,
+      grid: {
+        rows: 2,
+        fill: 'row',
+      },
+    },
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev",
 	},
-
-	pagination: {
-		el: ".swiper-pagination",
-		clickable: true,
-	},
 });
+
 
 const swiper1 = new Swiper(".swiper_review", {
 
